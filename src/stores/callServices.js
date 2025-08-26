86d04cf1883f715a8eb72ce23fdbb938db7d17ca
cherish-vuppala <your_email@example.com>
2025-08-26 12:01:16 +0530

    refactored callServices

--- src/stores/callServices.js
+++ src/stores/callServices.js
@@ -1,13 +1,22 @@
 import { defineStore } from "pinia";
-import { api } from "src/boot/axios";
-import { addForm } from "src/components/dynamicForm/indexDBService";
+import { api } from "src/boot/axios"; // Axios instance (configured globally)
+import { addForm } from "src/components/dynamicForm/indexDBService"; // IndexedDB utility for storing forms locally
+
+// Define Pinia store: callService
 export const callService = defineStore("callService", {
+  // --------- STATE (reactive data store) ---------
   state: () => ({
-    callServiceInfo: {},
-    callServicereport: {},
-    seviceList: [],
+    callServiceInfo: {}, // Stores detailed info about a particular service
+    callServicereport: {}, // Stores service report data (like counts/statistics)
+    seviceList: [], // Stores list of services assigned to engineer/dealer
   }),
+
+  // --------- ACTIONS (async operations / API calls) ---------
   actions: {
+    /**
+     * Fetch list of services assigned to a Service Engineer.
+     * API: /auth/css/serviceEngg/getServiceList
+     */
     async getEngServiceList() {
       let obj = {
         dealer: "",
@@ -15,90 +24,114 @@ export const callService = defineStore("callService", {
         requestNO: "",
         servEnggName: "",
       };
-      return api
-        .post("/auth/css/serviceEngg/getServiceList", obj)
-        .then((res) => {
-          console.log(res.data);
-          this.callServiceInfo = res.data;
-          return res.data;
-        })
-        .catch((err) => {
-          console.log(err);
-          throw err;
-        });
-    },
-    callServicesInfo(data) {
-      return api
-        .post("/auth/css/dealer/getServiceInfo", data)
-        .then((res) => {
-          console.log(res.data);
-          this.callServiceInfo = res.data;
-          return res.data;
-        })
-        .catch((err) => {
-          console.log(err);
-          throw err;
-        });
-    },
-    callServicesReport(data) {
-      return api
-        .post("/auth/css/dealer/getCountByDefType", data)
-        .then((res) => {
-          console.log(res.data);
-          return res.data;
-        })
-        .catch((err) => {
-          console.log(err);
-          throw err;
-        });
-    },
-    fetchServiceInfo(data) {
-      return api
-        .post("/auth/css/dealer/getServiceInfoDet", data)
-        .then((res) => {
-          console.log(res.data);
-          this.seviceList = res.data["ServiceDetails"];
-          return res.data["ServiceDetails"];
-        })
-        .catch((err) => {
-          console.log(err);
-          throw err;
-        });
+      let res = await api.post("/auth/css/serviceEngg/getServiceList", obj);
+      try {
+        console.log(res.data);
+        this.seviceList = res.data.ServiceDetails; // Update state with response
+        return res.data.ServiceDetails;
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
     },
 
-    actOnRequest(data) {
-      return api
-        .post("/auth/css/serviceEngg/actOnRequest", data)
-        .then((res) => {
-          localStorage.setItem("message", res.data.Message);
-          console.log(res.data);
-
-          addForm(JSON.stringify(res.data));
-          return res.data;
-        })
-        .catch((err) => {
-          console.log(err);
-          throw err;
-        });
+    /**
+     * Fetch service info details for a dealer.
+     * API: /auth/css/dealer/getServiceInfo
+     */
+    async callServicesInfo(data) {
+      let res = await api.post("/auth/css/dealer/getServiceInfo", data);
+      try {
+        console.log(res.data);
+        this.callServiceInfo = res.data; // Store service info in state
+        return res.data;
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
     },
-    UploadImage(data) {
-      return api
-        .post("/auth/css/fsr/image", data)
-        .then((res) => {
-          console.log("img upload", res);
 
-          return res.data;
-        })
-        .catch((err) => {
-          console.log(err);
-          throw err;
-        });
+    /**
+     * Fetch service report based on defect type (counts/statistics).
+     * API: /auth/css/dealer/getCountByDefType
+     */
+    async callServicesReport(data) {
+      let res = await api.post("/auth/css/dealer/getCountByDefType", data);
+      try {
+        console.log(res.data);
+        this.callServicereport = res.data; // Store report in state
+        return res.data;
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
+    },
+
+    /**
+     * Fetch detailed service info (extended details).
+     * API: /auth/css/dealer/getServiceInfoDet
+     */
+    async fetchServiceInfo(data) {
+      let res = await api.post("/auth/css/dealer/getServiceInfoDet", data);
+      try {
+        console.log(res.data);
+        // NOTE: Not saving in state, just returning data
+        return res.data;
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
+    },
+
+    /**
+     * Perform action on a service request (e.g., approve/reject/update).
+     * API: /auth/css/serviceEngg/actOnRequest
+     * - Stores message in localStorage
+     * - Saves data in IndexedDB using addForm()
+     */
+    async actOnRequest(data) {
+      try {
+        const res = await api.post("/auth/css/serviceEngg/actOnRequest", data);
+
+        // Save server message in localStorage (for UI alerts later)
+        localStorage.setItem("message", res.data.Message);
+
+        console.log(res.data);
+
+        // Save API response locally in IndexedDB
+        addForm(JSON.stringify(res.data));
+
+        return res.data;
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
+    },
+
+    /**
+     * Upload an image related to a service.
+     * API: /auth/css/fsr/image
+     */
+    async UploadImage(data) {
+      try {
+        const res = await api.post("/auth/css/fsr/image", data);
+        console.log("img upload", res);
+        return res.data;
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
     },
   },
+
+  // --------- GETTERS (read-only computed data from state) ---------
   getters: {
+    // Get list of services
     getserviceList() {
       return this.seviceList;
     },
+
+    // Get detailed service info
     getserviceEngList() {
       return this.callServiceInfo;
     },
