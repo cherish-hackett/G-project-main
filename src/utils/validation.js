c35c6102ae379e03547a606d676f55f9186aff87
cherish-vuppala <your_email@example.com>
2025-09-22 12:13:41 +0530

    fixed email validation error

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,9 +1,34 @@
 // src/utils/validation.js
 
 export const validateField = (field, value) => {
+  const mobileFields = new Set([
+    "COMPREQ_MOB_NO",
+    "GCLREP_MOB_NO",
+    "COMPDET_CALLER_NO",
+    "COMPDET_CUST_NO",
+    "SITEDET_MOB_NO",
+    "CUSTREP_MOB",
+    "PRECOMM_MOB_NO",
+    "CUST_REP_MOB_NO",
+    "GCL_REP_MOB_NO",
+    "SITE_MOB_NO",
+    "REQDET_REP_MOB",
+    "REQDET_MOB_NO",
+    "REQDET_CALLER_MOB",
+    "CUSTREP_MOB_NO",
+    "GCLREP_MOB",
+    "SITEDET_MOB",
+  ]);
+
   let err = "";
 
-  // Skip read-only fields
+  // Skip read-only fields4\
+  console.log(
+    `field from validateField field =  ${field.fieldName}, val = ${field.fieldValue}, type = ${field.fieldType}`
+  );
+  console.log(
+    `field from validateField [minVal =  ${field.fieldMinVal}, maxVal = ${field.fieldMaxVal}, length = ${field.fieldLength}]`
+  );
   if (field.isfieldEditable === "F") {
     return "";
   }
@@ -19,18 +44,37 @@ export const validateField = (field, value) => {
       if (isNaN(value)) {
         err = `Please enter a valid number for ${field.fieldName}.`;
       } else {
-        if (field.fieldMinVal && Number(value) < Number(field.fieldMinVal)) {
-          err = `${field.fieldName} must be ≥ ${field.fieldMinVal}.`;
+        console.log(typeof field.fieldId);
+        console.log("fieldId: ", field.fieldId);
+
+        console.log("mobileFields has field", mobileFields.has(field.fieldId));
+        if (mobileFields.has(field.fieldId)) {
+          console.log(typeof value);
+
+          if (Number(field.fieldLength) !== value.toString().length) {
+            console.log("field length: ", value.toString().length);
+
+            err = `${field.fieldName} must be exactly 10`;
+          }
+        } else {
+          if (field.fieldMinVal && Number(value) < Number(field.fieldMinVal)) {
+            err = `${field.fieldName} must be ≥ ${field.fieldMinVal}.`;
+          }
+          if (field.fieldMaxVal && Number(value) > Number(field.fieldMaxVal)) {
+            console.log(
+              "Phone number: ",
+              Number(value) > Number(field.fieldMaxVal)
+            );
+
+            err = `${field.fieldName} must be ≤ ${field.fieldMaxVal}.`;
+          }
         }
-        if (field.fieldMaxVal && Number(value) > Number(field.fieldMaxVal)) {
-          err = `${field.fieldName} must be ≤ ${field.fieldMaxVal}.`;
-        }
-        if (
-          field.fieldLength &&
-          value.toString().length !== Number(field.fieldLength)
-        ) {
-          err = `${field.fieldName} must be exactly ${field.fieldLength} digits.`;
-        }
+        // if (
+        //   field.fieldLength &&
+        //   value.toString().length !== Number(field.fieldLength)
+        // ) {
+        //   err = `${field.fieldName} must be exactly ${field.fieldLength} digits.`;
+        // }
       }
       break;
 
@@ -75,6 +119,9 @@ export const validateForm = (formData) => {
   formData.FSRdetails.forEach((tab) => {
     tab.fsrFieldDetList.forEach((field) => {
       const val = field.fieldValue; // 👈 use the bound value directly
+      console.log(
+        `tab = ${tab?.tabName}, field = ${field.fieldName}, val = ${val} from validate form`
+      );
       const err = validateField(field, val);
 
       if (err) {
